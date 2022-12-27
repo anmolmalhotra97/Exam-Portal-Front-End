ec494844bdbfacd9bddfd867df6d3d115f779fe6
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-12-26 23:40:28 -0500

    #8 modifying login component to support jwt authentication from backend

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login-service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {
+    username: '',
+    password: ''
+  };
+
+  constructor(
+    private matSnackBar: MatSnackBar,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  loginFormSubmit() {
+    console.log("Login Button Clicked");
+    /* 
+      * Validation in Typescript to check if Username or Password is Empty.
+      * MatSnackBar is used to alert the user.
+    */
+    if (this.loginData.username.trim() == '' || this.loginData.username == null) {
+      this.matSnackBar.open('Username is Required!!', '', {
+        duration: 3000,
+      });
+      return;
+    }
+
+    if (this.loginData.password.trim() == '' || this.loginData.username == null) {
+      this.matSnackBar.open('Password is Required!!', '', {
+        duration: 3000,
+      });
+      return;
+    }
+
+    /*
+    * Send Request to Backend Server to Login
+    */
+    this.loginService.generateToken(this.loginData).subscribe(
+      (data: any) => {
+        console.log('success');
+        console.log(data);
+      }, (error) => {
+        console.log("Error!!");
+        console.log(error);
+      }
+    )
+
   }
 
 }
