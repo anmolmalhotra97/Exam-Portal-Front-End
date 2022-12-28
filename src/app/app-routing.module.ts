67f66337ff0e987436e9066de10062adf45ea41b
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-12-28 00:40:49 -0500

    #12 Fixed path for Admin Dashboard Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './components/pages/admin/dashboard/dashboard.component';
+import { AdminDashboardComponent } from './components/pages/admin/admin-dashboard/admin-dashboard.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { UserDashboardComponent } from './components/pages/normal-user/user-dashboard/user-dashboard.component';
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'signup', component: RegisterComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  { path: 'admin', component: DashboardComponent, pathMatch: 'full' },
+  { path: 'admin', component: AdminDashboardComponent, pathMatch: 'full' },
   { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full' }
 ];
 
