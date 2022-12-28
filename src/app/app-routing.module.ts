9ce65cd9dea8d8d0f9861686d2da0131feded580
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-12-28 00:59:10 -0500

    #14 Added Routing Guard for Admin and Normal User Roles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,37 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { UserDashboardComponent } from './components/pages/normal-user/user-dashboard/user-dashboard.component';
 import { RegisterComponent } from './components/pages/register/register.component';
+import { AdminGuard } from './services/guard/admin/admin.guard';
+import { NormalUserGuard } from './services/guard/normal-user/normal-user.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'signup', component: RegisterComponent, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  { path: 'admin', component: AdminDashboardComponent, pathMatch: 'full' },
-  { path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full' }
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'signup',
+    component: RegisterComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin',
+    component: AdminDashboardComponent,
+    pathMatch: 'full',
+    canActivate: [AdminGuard]
+  },
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent,
+    pathMatch: 'full',
+    canActivate: [NormalUserGuard]
+  }
 ];
 
 @NgModule({
