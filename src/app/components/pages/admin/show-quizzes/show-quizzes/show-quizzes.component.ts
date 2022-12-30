b7447c1af695798eabd61a0b4035a27053d7db85
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-12-30 02:04:29 -0500

    #28 #29 Delete API Integration with Delete Confirmation Prompt

--- src/app/components/pages/admin/show-quizzes/show-quizzes/show-quizzes.component.ts
+++ src/app/components/pages/admin/show-quizzes/show-quizzes/show-quizzes.component.ts
@@ -40,4 +40,36 @@ export class ShowQuizzesComponent implements OnInit {
     );
   }
 
+  public deleteQuiz(quizId: any) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this quiz!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        this.quizService.deleteQuiz(quizId).subscribe(
+          (data: any) => {
+            console.log(data);
+            //There are two ways to update the quizzes list on the page
+            //
+            // 1. Call the ngOnInit() method again
+            // This will fetch the latest quizzes list from the server
+            // this.ngOnInit();
+            //
+            // 2. Filter the quizzes list to remove the deleted quiz
+            // This will remove the deleted quiz from the quizzes list, without involving the server again.
+            this.quizzes = this.quizzes.filter((quiz: any) => quiz.quizId !== quizId);
+            Swal.fire('Success', 'Quiz deleted successfully', 'success');
+          },
+          (error: any) => {
+            console.log(error);
+            Swal.fire('Error', 'Error while deleting quiz', 'error');
+          }
+        );
+      }
+    });
+  }
 }
